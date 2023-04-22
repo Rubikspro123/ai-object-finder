87f4b1a550f455650530ed8a58483bc409f83e13
Rubikspro123 <87181104+Rubikspro123@users.noreply.github.com>
2023-04-22 18:27:11 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+status ='';
+input = '';
 function setup(){
     canvas = createCanvas(360, 240);
     canvas.center();
@@ -6,5 +8,16 @@ function setup(){
     video.hide()
     
 }
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById('status').innerHTML = 'Status : Detecting Objects';
+    input = document.getElementById('input').value;
+}
+function modelLoaded(){
+console.log('Model Loaded!');
+status = true;
+}
+
 function draw(){
-    image(video, 0, 0, 360,240);}
+    image(video, 0, 0, 360,240);
+}
